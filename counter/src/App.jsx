15671df5c7a60bc77c60d36ec154983dd077736d
HARI-257 <88844719+HARI-257@users.noreply.github.com>
2025-03-06 12:04:19 +0530

    Add files via upload

---
+++ counter/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  // let counter = 15
+  const addValue = () => {
+    setCounter(counter + 1);
+    // setCounter((prevCounter) => prevCounter + 1)
+    // setCounter((prevCounter) => prevCounter + 1)
+    // setCounter((prevCounter) => prevCounter + 1)
+    // setCounter((prevCounter) => prevCounter + 1)
+  };
+
+  const removevalue = () => {
+    setCounter(counter - 1);
+  };
+  return (
+    <>
+      <h1>Counter App {counter}</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add value</button>{" "}
+      <button onClick={removevalue}>remove value</button>
+      <p>footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
